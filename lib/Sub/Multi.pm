127db280b61b6bc91c8e3061f7b339de69b16a53
Chia-liang Kao (高嘉良) <clkao@clkao.org>
2006-07-10 22:44:14 +0000

    Import of CLKAO/Sub-Multi-0.001_2 from CPAN.
    gitpan-cpan-distribution: Sub-Multi
    gitpan-cpan-version:      0.001_2
    gitpan-cpan-path:         CLKAO/Sub-Multi-0.001_2.tar.gz
    gitpan-cpan-author:       CLKAO
    gitpan-cpan-maturity:     developer

--- lib/Sub/Multi.pm
+++ lib/Sub/Multi.pm
@@ -1,7 +1,7 @@
 package Sub::Multi;
-our $VERSION = '0.001_1';
+our $VERSION = '0.001_2';
 use base 'Class::Multimethods::Pure';
-use Data::Bind 0.26;
+use Data::Bind 0.27;
 
 =head1 NAME
 
@@ -9,19 +9,66 @@ Sub::Multi - Data::Bind-based multi-sub dispatch
 
 =head1 SYNOPSIS
 
- my $wrapper = Sub::Multi->new($sub1, $sub2, $sub3);
+ my $multi_sub = Sub::Multi->new($sub1, $sub2, $sub3);
 
- $wrapper->([\1,\3], { named => \'name'});
+Now dispatch to the right subroutine, based on C<@args>.
+
+ $multi_sub->(@args);
 
 =head1 DESCRIPTION
 
+Perl6 allows multiple subs and methods with the same name, differing only in
+their signature.
 
+    multi sub bar (Dog $foo) {?}
+    multi sub bar (Cat $foo) {?}
+
+Dispatching will happen based on the runtime signature of the subroutine or
+method call. 
+
+=head2 new
+
+ my $multi_sub = Sub::Multi->new($sub1, $sub2, $sub3);
+ $multi_sub->(@args);
+
+Build and return a code reference that will dispatch based on the Perl6
+multi dispatch semantics. 
+
+I<TODO: Verify this statement:> Before the method is actually dispatched, 
+a call to Data::Bind->sub_signature should be made to register the subroutine
+signature. 
 
 =cut
 
+
 sub new {
     my ($class, @subs) = @_;
-    return sub { $class->dispatch(\@subs, @_) };
+    return bless sub { $class->dispatch(\@subs, @_) }, 'Sub::Multi::Method';
+}
+
+=head2 add_multi
+
+  my $multi_sub  =  Sub::Multi->add_multi($sub_name, \&sub );
+  $multi_sub->(@args);
+
+Associates C<$sub_name> with C<\&sub>, and returns code reference
+that will dispatch appropriately. C<add_multi> can be called multiple
+times with the same C<$sub_name> to build a multi-dispatch method. 
+
+I<TODO: Verify this statement:> Before the method is actually dispatched, 
+a call to Data::Bind->sub_signature should be made to register the subroutine
+signature. 
+
+=cut
+
+sub add_multi {
+    my ($class, $name, $sub) = @_;
+    my $pkg = ((caller)[0]);
+    no strict 'refs';
+    my $subs = ${$pkg."::SUB_MULTI_REGISTRY"} ||= [];
+    push @$subs, $sub;
+    no warnings 'redefine';
+    *{$pkg."::$name"} = $class->new(@$subs);
 }
 
 sub dispatch {
@@ -40,6 +87,12 @@ sub dispatch {
 }
 
 1;
+
+=head1 SEE ALSO
+
+L<Data::Bind>
+
+B<TODO: > Add a good reference to Perl6 multiple dispatch here. 
 
 =head1 AUTHORS
 
